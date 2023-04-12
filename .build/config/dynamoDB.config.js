1ce06d4239e0b95d62a0de656a4f8b5075ab1a6b
Rafael Ferraz Rodrigues <rferrazrodrigues@gmail.com>
2023-04-12 01:13:54 -0300

    initial commit

--- .build/config/dynamoDB.config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dynamoDB = void 0;
-var dynamoose = require("dynamoose");
-dynamoose.aws.ddb.local();
-exports.dynamoDB = dynamoose;
-//# sourceMappingURL=dynamoDB.config.js.map
